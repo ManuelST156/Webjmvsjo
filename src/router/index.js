6b46b04e98f53c9fbc62abd3d677d199da864cf8
ManuelST156 <manuelsancheztejada25@gmail.com>
2024-06-10 17:52:42 -0400

    SideBar Colocado

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import SettingsView from '@/views/SettingsView.vue'
+import VacanciesView from '@/views/VacanciesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +13,19 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
+      path: '/acercadenosotros',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutView
+    },
+    {
+      path: '/vacantes',
+      name: 'vacancies',
+      component: VacanciesView
+    },
+    {
+      path: '/ajustes',
+      name: 'settings',
+      component: SettingsView
     }
   ]
 })
